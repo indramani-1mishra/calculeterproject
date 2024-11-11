75ede758d8eb5d60238c6fb2fc8d38bc6883b299
RAJ <rajmishra.ydd@gmail.com>
2024-11-11 15:47:56 +0530

    css and js code add

---
+++ calculeter.js
@@ -0,0 +1,61 @@
+let number='';
+let number1=number;
+     
+
+     function appendZero() {
+        number += '0'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+   
+   
+    function appendnumber1() {
+        number += '1'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber2() {
+        number += '2'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber3() {
+        number += '3'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber4() {
+        number += '4'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+
+    function appendnumber5() {
+        number += '5'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+
+    function appendnumber6() {
+        number += '6'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber6() {
+        number += '6'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber6() {
+        number += '6'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber7() {
+        number += '7'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber8() {
+        number += '8'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function appendnumber9() {
+        number += '9'; // Append '0' to the current number
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    function clearDisplay() {
+        number = ''; // Reset the number to empty
+        document.getElementsByClassName('dis')[0].value = number; // Update the input box value
+    }
+    
